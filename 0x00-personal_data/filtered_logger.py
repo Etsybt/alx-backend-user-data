d0eed43c1ee03100e146431fb571946fc68a69c3
Etsybt <nouhailahmidouch2@gmail.com>
2024-07-31 13:49:42 +0000

    update

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -3,6 +3,7 @@
 filtered_logger
 """
 import re
+import logging
 
 
 def filter_datum(fields, redaction, message, separator):
@@ -14,3 +15,21 @@ def filter_datum(fields, redaction, message, separator):
                          lambda m: f'{m.group().split("=")[0]}={redaction}',
                          message)
     return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        record.msg = filter_datum(
+            self.fields, self.REDACTION, record.getMessage(), self.SEPARATOR)
+        return super().format(record)
