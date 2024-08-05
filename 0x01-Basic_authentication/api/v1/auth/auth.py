8e083db9f3dfebf6e4bf2e1fbfd495fb9f4c72e0
Etsybt <hmidouchnouhaila2@gmail.com>
2024-08-05 16:26:59 +0000

    update

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -10,10 +10,16 @@ class Auth:
     """class to manage the API authentification
     """
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """ resuire_auth method
+        """
         return False
 
     def authorization_header(self, request=None) -> str:
+        """ auth_header method
+        """
         return None
 
     def current_user(self, request=None) -> TypeVar('User'):
+        """ current_user method
+        """
         return None
