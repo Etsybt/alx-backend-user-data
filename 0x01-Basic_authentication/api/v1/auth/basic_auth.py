26e0a2ca65beef8699d1e652fd2f2c3f58376a4e
Etsybt <hmidouchnouhaila2@gmail.com>
2024-08-05 17:40:19 +0000

    update

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -8,4 +8,11 @@ from api.v1.auth.auth import Auth
 class BasicAuth(Auth):
     """ Basic authentication class
     """
-    pass
+    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+        """ Extract Base64 part of the Authorization header for Basic Authentication
+        """
+        if not isinstance(authorization_header, str):
+            return None
+        if not authorization_header.startswith("Basic "):
+            return None
+        return authorization_header[6:]
