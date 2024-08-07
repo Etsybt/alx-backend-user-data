b4e200075d4f21c279a56005aa53e0ccc3df17cb
Etsybt <hmidouchnouhaila2@gmail.com>
2024-08-07 15:08:11 +0000

    update

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -58,8 +58,7 @@ def before_request():
         '/api/v1/auth_session/login/'
     ]
     if auth.require_auth(request.path, excluded_paths):
-        if auth.authorization_header(request) is None and auth.session_cookie(
-                request) is None:
+        if auth.authorization_header(request) is None and auth.session_cookie(request) is None:
             abort(401)
         request.current_user = auth.current_user(request)
         if request.current_user is None:
