45c82133f8991249b5f5b0ab0103e4fccb519d5c
Etsybt <nouhailahmidouch2@gmail.com>
2024-08-13 20:42:24 +0000

    update

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -5,10 +5,7 @@ auth file
 import bcrypt
 from db import DB
 from user import User
-<<<<<<< HEAD
-=======
 from sqlalchemy.orm.exc import NoResultFound
->>>>>>> 0240b9084dfc61af4141f9ac7be06e62fec9d8a7
 
 
 def _hash_password(password: str) -> bytes:
