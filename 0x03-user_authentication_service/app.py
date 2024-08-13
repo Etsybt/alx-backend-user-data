f4a4fbd2fe0b2f1c80b7f9c0f30cefbaa7d84487
Etsybt <nouhailahmidouch2@gmail.com>
2024-08-13 20:54:52 +0000

    update

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -2,14 +2,31 @@
 """
 a simple Flask app
 """
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from auth import Auth
 
 app = Flask(__name__)
+AUTH = Auth()
 
 
 @app.route("/", methods=["GET"])
 def index():
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route("/users", methods=["POST"])
+def register_user():
+    email = request.form.get("email")
+    password = request.form.get("password")
+
+    if not email or not password:
+        return jsonify({"message": "Missing email or password"}), 400
+
+    try:
+        user = AUTH.register_user(email, password)
+        return jsonify({"email": user.email, "message": "user created"}), 201
+    except ValueError as e:
+        return jsonify({"message": str(e)}), 400
 
 
 if __name__ == "__main__":
