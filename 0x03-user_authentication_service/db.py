ffd48c45ca3161e5f370970f62e5095ea9d5527a
Etsybt <nouhailahmidouch2@gmail.com>
2024-08-13 11:10:30 +0000

    update

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -5,6 +5,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
+from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.exc import InvalidRequestError
 from user import Base, User
 
 
@@ -36,3 +38,16 @@ class DB:
         self._session.add(new_user)
         self._session.commit()
         return new_user
+
+    def find_user_by(self, **kwargs) -> User:
+        """ takes in arbitrary keyword arguments and returns
+        the first row found in the users table as filtered
+        by the method’s input arguments.
+        """
+        try:
+            user = self._session.query(User).filter_by(**kwargs).one()
+            return user
+        except NoResultFound:
+            raise NoResultFound("No user found with the given criteria")
+        except InvalidRequestError:
+            raise InvalidRequestError("Invalid query arguments")
