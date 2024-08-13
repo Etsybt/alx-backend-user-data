27a05c6814516164671350ca5aa6af98300021dc
Etsybt <hmidouchnouhaila2@gmail.com>
2024-08-13 20:05:10 +0000

    app file update

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -2,29 +2,23 @@
 """
 app file
 """
-from flask import Flask, jsonify, request
+from flask import Flask, request, jsonify
 from auth import Auth
-
+from sqlalchemy.exc import IntegrityError
 
 app = Flask(__name__)
 AUTH = Auth()
 
 
-@app.route("/", methods=["GET"], strict_slashes=False)
-def index():
-    """GET route that returns a JSON payload"""
-    return jsonify({"message": "Bienvenue"})
-
-
-@app.route("/users", methods=["POST"], strict_slashes=False)
-def users() -> str:
-    """POST /users route to register a user"""
+@app.route('/users', methods=['POST'])
+def users():
+    """Register a new user."""
     email = request.form.get('email')
     password = request.form.get('password')
 
     try:
         user = AUTH.register_user(email, password)
-        return jsonify({"email": user.email, "message": "user created"})
+        return jsonify({"email": user.email, "message": "user created"}), 200
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
 
