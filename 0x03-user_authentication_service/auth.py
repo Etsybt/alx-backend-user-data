d123861ce35b156327527214785cbe29cd17d7ed
Etsybt <nouhailahmidouch2@gmail.com>
2024-08-13 11:33:13 +0000

    update

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -2,11 +2,33 @@
 """
 auth file
 """
+from db import DB
+from user import User
+from sqlalchemy.exc import NoResultFound
 import bcrypt
 
 
-def _hash_password(password: str) -> bytes:
-    """returns a salted hash of the input password
+class Auth:
+    """Auth class to interact with the authentication database.
     """
-    hashed = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
-    return hashed
+
+    def __init__(self):
+        self._db = DB()
+
+    def _hash_password(self, password: str) -> bytes:
+        """returns a salted hash of the input password
+        """
+        hashed = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+        return hashed
+
+    def register_user(self, email: str, password: str) -> User:
+        """registers a new user
+        """
+        try:
+            self._db.find_user_by(email=email)
+            raise ValueError(f"User {email} already exists")
+        except NoResultFound:
+            hashed_password = self._hash_password(password)
+            user = self._db.add_user(
+                email=email, hashed_password=hashed_password)
+            return user
