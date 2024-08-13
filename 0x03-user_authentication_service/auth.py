5a7fccb56e78e3c8beb8ed4a15ffafd897562db1
Etsybt <nouhailahmidouch2@gmail.com>
2024-08-13 11:50:08 +0000

    FIXED HASH METHOD

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -8,18 +8,19 @@ from sqlalchemy.exc import NoResultFound
 import bcrypt
 
 
+def _hash_password(password: str) -> bytes:
+    """returns a salted hash of the input password
+    """
+    hashed = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+    return hashed
+
+
 class Auth:
     """Auth class to interact with the authentication database.
     """
 
     def __init__(self):
         self._db = DB()
-
-    def _hash_password(self, password: str) -> bytes:
-        """returns a salted hash of the input password
-        """
-        hashed = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
-        return hashed
 
     def register_user(self, email: str, password: str) -> User:
         """registers a new user
@@ -28,7 +29,7 @@ class Auth:
             self._db.find_user_by(email=email)
             raise ValueError(f"User {email} already exists")
         except NoResultFound:
-            hashed_password = self._hash_password(password)
-            user = self._db.add_user(
-                email=email, hashed_password=hashed_password)
-            return user
+            pass
+        hashed_password = _hash_password(password)
+        user = self._db.add_user(email, hashed_password)
+        return user
